2bae8f92c7bebd99113d885a2fbc8eb2bb79e6fc
Yash Kumar Rana <2200520100072@ietlucknow.ac.in>
2024-11-13 16:33:05 +0530

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
+import 'dotenv/config';
 import express from 'express';
-const app = express()
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-    
-})
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(error => console.error('MongoDB connection error:', error));
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
